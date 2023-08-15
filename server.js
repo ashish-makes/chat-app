0f512c92be66bea2f7ceb8eac4fb7011ebf45adf
ashish-makes <ashish.makes@gmail.com>
2023-08-15 12:49:33 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
